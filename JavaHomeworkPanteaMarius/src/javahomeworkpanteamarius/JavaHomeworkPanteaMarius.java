530c3cf0d5449ecfa85b8dce89c7d3f4c3293946
admin <admin@panteas-macbook>
2016-11-08 22:21:13 +0200

    First commit

--- JavaHomeworkPanteaMarius/src/javahomeworkpanteamarius/JavaHomeworkPanteaMarius.java
+++ JavaHomeworkPanteaMarius/src/javahomeworkpanteamarius/JavaHomeworkPanteaMarius.java
@@ -1,9 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is a a java program that will use scanner class to read from keyboard and print it in console
  */
 package javahomeworkpanteamarius;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +15,8 @@ public class JavaHomeworkPanteaMarius {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name please:" );  
+        String username = (scan.nextLine());
+        System.out.println("Your name is " + username);
     }
-    
-}
